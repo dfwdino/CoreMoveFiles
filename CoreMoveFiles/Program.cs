f0bd473c7e4a5779c585a38d310a9071d1192819
shane newsom <shane.newsom@gmail.com>
2019-09-26 12:05:30 -0500

    Add logic to test for same file.

--- CoreMoveFiles/Program.cs
+++ CoreMoveFiles/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.VisualBasic.FileIO;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Xml.Serialization;
@@ -19,8 +20,11 @@ namespace CoreMoveFiles
             }
             else
             {
-                moveLocations.Add(new MoveLocation() { SouceLocation = @"C:\Users\Shane\Desktop\tempphtos\temp phone\Freddy", DesctionLocation = @"E:\Personal\Images\Clean\People\Freddy Lee" });
-                moveLocations.Add(new MoveLocation() { SouceLocation = @"C:\Users\Shane\Desktop\tempphtos\temp phone\Elliott", DesctionLocation = @"E:\Personal\Images\Clean\People\Elliot Ryker Wolke Newsom" });
+                moveLocations.Add(new MoveLocation() { SouceLocation = @"C:\Users\Shane\Desktop\tempphtos\temp phone\Freddy", 
+                                                        DesctionLocation = @"E:\Personal\Images\Clean\People\Freddy Lee" });
+
+                moveLocations.Add(new MoveLocation() { SouceLocation = @"C:\Users\Shane\Desktop\tempphtos\temp phone\Elliott", 
+                                                        DesctionLocation = @"E:\Personal\Images\Clean\People\Elliot Ryker Wolke Newsom" });
 
                 XmlSerializer xs = new XmlSerializer(moveLocations.GetType());
                 TextWriter tw = new StreamWriter(xmldataloction);
@@ -31,6 +35,43 @@ namespace CoreMoveFiles
 
             foreach (MoveLocation imagemove in moveLocations)
             {
+                if (File.Exists(imagemove.SouceLocation))
+                {
+                    Console.WriteLine($"Can't find file {imagemove.SouceLocation}. Skipping folder.");
+                    continue;
+                }
+                else if(File.Exists(imagemove.DesctionLocation))
+                {
+                    Console.WriteLine($"Can't find file {imagemove.DesctionLocation}. Skipping folder.");
+                    File.Create(imagemove.DesctionLocation);
+                }
+
+                var AllFiles = Directory.GetFiles(imagemove.SouceLocation);
+
+                foreach (string filetomove in AllFiles)
+                {
+                    FileInfo fi = new FileInfo(filetomove);
+                    string fullnamedesc = imagemove.DesctionLocation + "\\" + fi.Name;
+
+                    if (File.Exists(fullnamedesc))
+                    {
+                        FileInfo fid = new FileInfo(fullnamedesc);
+
+                        if (fi.Length.Equals(fid.Length))
+                        {
+                            Console.WriteLine($"Found File {fullnamedesc}. Deleting file.");
+                            FileSystem.DeleteFile(fullnamedesc,UIOption.OnlyErrorDialogs,RecycleOption.SendToRecycleBin);
+                        }
+                        else
+                        {
+                            fullnamedesc = fullnamedesc.Replace(".", $"_{DateTime.Now.ToFileTime()}.");
+                        }
+                        
+                    }
+
+                    File.Move(filetomove, fullnamedesc);
+
+                }
 
             }
            
@@ -50,7 +91,7 @@ namespace CoreMoveFiles
             }
             catch (Exception ex)
             {
-                //ExceptionLogger.WriteExceptionToConsole(ex, DateTime.Now);
+                Console.WriteLine(ex.Message);
             }
             return returnObject;
         }
