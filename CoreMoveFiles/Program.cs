850dfedab8696fe4605657a330819bd099ed7c71
shane newsom <shane.newsom@gmail.com>
2019-09-26 10:26:24 -0500

    Reading xml file

--- CoreMoveFiles/Program.cs
+++ CoreMoveFiles/Program.cs
@@ -14,7 +14,8 @@ namespace CoreMoveFiles
 
             if (File.Exists(xmldataloction))
             {
-
+                moveLocations = DeserializeXMLFileToObject<List<MoveLocation>>(xmldataloction);
+                
             }
             else
             {
@@ -34,6 +35,24 @@ namespace CoreMoveFiles
             }
            
 
+        }
+
+        public static T DeserializeXMLFileToObject<T>(string XmlFilename)
+        {
+            T returnObject = default(T);
+            if (string.IsNullOrEmpty(XmlFilename)) return default(T);
+
+            try
+            {
+                StreamReader xmlStream = new StreamReader(XmlFilename);
+                XmlSerializer serializer = new XmlSerializer(typeof(T));
+                returnObject = (T)serializer.Deserialize(xmlStream);
+            }
+            catch (Exception ex)
+            {
+                //ExceptionLogger.WriteExceptionToConsole(ex, DateTime.Now);
+            }
+            return returnObject;
         }
     }
 }
